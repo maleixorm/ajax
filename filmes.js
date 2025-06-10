ccef4dd94e49e88330cd69f19ca2b2ee4c5104a4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-10 00:20:15 -0300

    Utilizando a função DomParser e o Script XMLtoJSON

--- filmes.js
+++ filmes.js
@@ -5,6 +5,11 @@ function getFilmes() {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             let XMLFilmes = xmlHttp.responseText;
             console.log(XMLFilmes);
+            let parser = new DOMParser();
+            let domFilmes = parser.parseFromString(XMLFilmes, "text/xml");
+            console.log(domFilmes);
+            let jsonFilmes = xmlToJson(domFilmes);
+            console.log(jsonFilmes);
         }
         
         if (xmlHttp.readyState == 4 && xmlHttp.status == 404) {
