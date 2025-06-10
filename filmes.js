601fdedad3697f3be36e92add95a58b264a46ea0
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-06-10 02:18:27 -0300

    Desenvolvendo a lógica para mostrar a lista de forma dinâmica

--- filmes.js
+++ filmes.js
@@ -4,12 +4,41 @@ function getFilmes() {
     xmlHttp.onreadystatechange = () => {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             let XMLFilmes = xmlHttp.responseText;
-            console.log(XMLFilmes);
             let parser = new DOMParser();
             let domFilmes = parser.parseFromString(XMLFilmes, "text/xml");
-            console.log(domFilmes);
             let jsonFilmes = xmlToJson(domFilmes);
-            console.log(jsonFilmes);
+            let filme = jsonFilmes['filmes']['filme'];
+            for (let i in filme) {
+                let lista = document.getElementById('lista');
+                let genero = jsonFilmes['filmes']['filme'][i]['genero'];
+                let generoFilme = '';
+                for (let g in genero) {
+                    if (generoFilme) {
+                        generoFilme += ', ';
+                    }
+                    generoFilme += genero[g]['#text'];
+                }
+                let elenco = jsonFilmes['filmes']['filme'][i]['elenco']['ator'];
+                let elencoFilme = '';
+                for (let l in elenco) {
+                    if (elencoFilme) {
+                        elencoFilme += ', ';
+                    }
+                    elencoFilme += elenco[l]['#text'];
+                }
+                lista.innerHTML += `
+                    <div class="row mt-5">
+                        <div class="col">
+                            <p><strong>Título:</strong> ${filme[i]['titulo']['#text']}</p>
+                            <p><strong>Resumo:</strong> ${filme[i]['resumo']['#text']}</p>
+                            <p><strong>Gênero:</strong> ${generoFilme}</p>
+                            <p><strong>Elenco:</strong> ${elencoFilme}</p>
+                            <p><strong>Data de lançamento:</strong> ${filme[i]['dataLancamento']['#text']}</p>
+                            <hr>
+                        </div>
+                    </div>
+                `;
+            }
         }
         
         if (xmlHttp.readyState == 4 && xmlHttp.status == 404) {
